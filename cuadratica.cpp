c18a892ac0ab91e7592388f42374be7b370d9d6e
Ramsesballesteros <badr080626hmnlzma7@cbtis217.edu.mx>
2024-10-17 11:40:05 -1200

    agg comm

--- cuadratica.cpp
+++ cuadratica.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main {
-    // aqui se muestran las variables a utilizar para calcular la distancia
+    // este programa te pide insertar 3 datos para luego procesarlos con la formula para calcular 
+    // distancia entre estos mismos tres datos, al final muestra el resultado con decimales
     double x1, y2, x2, y1,d;
     cout<<"programa para calcular la distancia"<<endl;
     cout<<"Autor: Ramses Ballesteros Diaz"<<endl;
@@ -16,10 +17,8 @@ int main {
     cin>>y2;
     cout<<endl;
 
-    // formula general para calcular la distancia con los datos ya incertados 
     d=sqrt( (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
-
-    // resultado despues de seguir todo el proceso  
+ 
     cout<<"la distancia entre los dos puntos es: "<<d<<endl;
 
 

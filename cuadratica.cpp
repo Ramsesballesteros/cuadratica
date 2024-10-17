91220d700877ea5531017eea3d1f4124cdad9c7c
Ramsesballesteros <badr080626hmnlzma7@cbtis217.edu.mx>
2024-10-17 11:07:46 -1200

    agg todo

---
+++ cuadratica.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main {
+    double x1, y2, x2, y1,d;
+    cout<<"programa para calcular la distancia"<<endl;
+    cout<<"Autor: Ramses Ballesteros Diaz"<<endl;
+    cout<<"x1: ";
+    cin>> x1;
+    cout<<"y1: ";
+    cin>>y1;
+    cout<<endl;
+    cout<<"x2: ";
+    cin>>x2;
+    cout<<"y2: ";
+    cin>>y2;
+    cout<<endl;
+
+    d=sqrt( (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+
+
+    cout<<"la distancia entre los dos puntos es: "<<d<<endl;
+
+
+return 0;
+}
+
